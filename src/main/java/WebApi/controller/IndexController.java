a907bfb4baca64fce963db311defe52950281c4d
Artem Usenko <usenko.job@gmail.com>
2017-05-02 17:42:31 +0300

    Deploy

--- src/main/java/WebApi/controller/IndexController.java
+++ src/main/java/WebApi/controller/IndexController.java
@@ -1,17 +1,27 @@
 package WebApi.controller;
 
+import WebApi.dao.UserDAO;
+import WebApi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by usenk on 02.05.2017.
  */
 @Controller
 public class IndexController {
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String index () {
+    @Autowired
+  private UserDAO userDAO;
 
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index (ModelMap map) {
+        List<User> byId = userDAO.findById(1);
+        map.put("userAll",byId);
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/page/index.jsp
+++ src/main/webapp/WEB-INF/page/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
 <!DOCTYPE html>
@@ -6,7 +7,12 @@
     <title>Title</title>
 </head>
 <body>
-<h1> Hello </h1>
 
+<h1> Hello </h1>
+<c:forEach var="user" items="userAll">
+ <c:out value="${user.name}"></c:out>
+ <c:out value="${user.username}"></c:out>
+ <c:out value="${user}"></c:out>
+</c:forEach>
 </body>
 </html>
