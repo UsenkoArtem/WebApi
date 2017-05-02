8ef035c9a4125291a397806fe1fff4c5c1081b6d
Artem Usenko <usenko.job@gmail.com>
2017-05-02 18:05:02 +0300

    Deploy

--- src/main/java/WebApi/webAppConfig/HibernateConfig.java
+++ src/main/java/WebApi/webAppConfig/HibernateConfig.java
@@ -11,7 +11,6 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
@@ -46,17 +45,14 @@ public class HibernateConfig {
 
         @Bean
         public BasicDataSource dataSource() throws URISyntaxException {
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            String username = System.getenv("JDBC_DATABASE_USERNAME");
+            String password = System.getenv("JDBC_DATABASE_PASSWORD");
 
             BasicDataSource basicDataSource = new BasicDataSource();
             basicDataSource.setUrl(dbUrl);
             basicDataSource.setUsername(username);
             basicDataSource.setPassword(password);
-
 
             return basicDataSource;
         }
